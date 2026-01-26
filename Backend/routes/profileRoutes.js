936ffb88e6b2eec0487e9affc85b4bbfa5b3f2fa
CSShubham <raj079097@gmail.com>
2026-01-27 01:48:34 +0530

    Profile routes modified

--- Backend/routes/profileRoutes.js
+++ Backend/routes/profileRoutes.js
@@ -3,7 +3,8 @@ import {
   getMyProfile,
   updateMyProfile,
 } from "../controllers/profileController.js";
-import { protect, authorize } from "../middlewares/authMiddleware.js";
+import { protect } from "../middlewares/authMiddleware.js";
+import { authorize } from "../middlewares/roleMiddleware.js";
 
 const router = express.Router();
 
