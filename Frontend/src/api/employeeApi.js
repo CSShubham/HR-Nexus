90ce351fc11b08afb6f4abb3eba8fb83d5507c17
CSShubham <raj079097@gmail.com>
2026-01-31 10:57:24 +0530

    employee api updated

--- Frontend/src/api/employeeApi.js
+++ Frontend/src/api/employeeApi.js
@@ -3,6 +3,7 @@ import api from './axiosConfig';
 export const employeeApi = {
   getAllEmployees: () => api.get('/employees'),
   getEmployeesWithWorkingStatus: () => api.get('/employees/working-status'),
+  updateEmployee: (employeeId, data) => api.put(`/employees/${employeeId}`, data),
   onboardEmployee: (candidateId, data) => api.post(`/employees/onboard/${candidateId}`, data),
   offboardEmployee: (employeeId, data) => api.post(`/employees/offboard/${employeeId}`, data),
   getOffboardingInfo: () => api.get('/employees/offboarding/me'),
